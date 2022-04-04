ed50a642bd150a18fa0c7c464c33f07b3d422c98
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-04-04 22:05:53 +0600

    Dashbord done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import "./Dashboard.css";
 
 const Dashboard = () => {
+  const [chartData, setChartData] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setChartData(data));
+  }, []);
+
   return (
-    <div>
-      <h2>This is dashboard</h2>
+    <div className="dashboard">
+      <LineChart width={400} height={500} data={chartData}>
+        <Line dataKey={"sell"}></Line>
+        <Line dataKey={"investment"}></Line>
+        <XAxis dataKey="month"></XAxis>
+        <Tooltip></Tooltip>
+        <YAxis></YAxis>
+      </LineChart>
+
+      <BarChart width={730} height={250} data={chartData}>
+        <CartesianGrid strokeDasharray="3 3"></CartesianGrid>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="sell" fill="#8884d8" />
+        <Bar dataKey="investment" fill="#82ca9d" />
+      </BarChart>
     </div>
   );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,12 +4,22 @@ import laptop from "../../Images/Laptop.png";
 
 const Home = () => {
   return (
-    <div className="home-body">
-      <div>
-        <h1>LAPTOP HOUSE</h1>
-        <h1 className="best-choice">Your Best Choice</h1>
+    <div>
+      <div className="home-body">
+        <div className="laptop-house">
+          <h1>LAPTOP HOUSE</h1>
+          <h1 className="best-choice">Your Best Choice</h1>
+          <p>
+            Configure Your Dream Computer To Run Ubuntu Flawlessly At Great
+            Prices! We Can Provide Effective Customised Computing Solutions To
+            Meet Any Of Your Requirements. 3 Year Warranty. Lifetime Support. 2
+            Day Build Time. Amenities: Ubuntu Powered, 2-3 Day Build Time, 3
+            Year Warranty.
+          </p>
+        </div>
+        <img src={laptop} alt="" />
       </div>
-      <img src={laptop} alt="" />
+      <h2>Customer Reviews(3)</h2>
     </div>
   );
 };
